6f0843c6e7f021a049b36eaccbee2558806ef58d
Calvin Chern <calvin.chern24@gmail.com>
2023-03-12 22:42:04 -0400

    Add reaction controller functions

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -72,3 +72,34 @@ module.exports = {
             )
             .catch((err) => res.status(500).json(err));
     },
+
+    // Creating a reaction
+    createReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "There was no thought found with this ID!" })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    
+    // Deleting a reaction
+    deleteReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "There was no thought with this ID!" })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+};
